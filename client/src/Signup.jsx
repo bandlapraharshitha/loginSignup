e8bc79c014ae781de9247ed165966a60f9d0826a
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-14 23:33:29 +0530

    minute changes in signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -61,7 +61,7 @@ const Signup = () => {
             type="password"
             required
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="••••••••"
+            placeholder="•••••••••"
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
