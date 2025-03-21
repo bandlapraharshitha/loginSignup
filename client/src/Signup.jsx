5f1c3a9edb172c0cadaadc5d8602979e4479aa37
bandlapraharshitha <praharshitha35@gmail.com>
2025-03-21 23:47:50 +0530

    changes

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -18,7 +18,7 @@ const Signup = () => {
   return (
     <div className= "min-h-screen flex items-center justify-center bg-gray-100">
     <div className= "bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-      <h2 className= "text-2xl font-bold text-center text-gray-800 mb-6"> Create an Account</h2>
+      <h2 className= "text-2xl font-bold text-center text-gray-800 mb-6"> Create an Account </h2>
       
       <form className="space-y-4" onSubmit={handleSubmit}>
         <div>
